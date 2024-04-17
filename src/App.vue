c7b9290498e50c402e7032e2572ee3c347dd7fd3
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-04-17 17:13:09 +0200

    feat: stampa card con richiesta axios

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <script>
 import axios from "axios";
 import { api, store } from "./store";
-// import AppHeader from "./components/AppHeader.vue";
+import CharacterCardList from "./components/CharacterCardList.vue";
 
 export default {
   data() {
     return {
       store,
       title: "GDR",
+      n_character: "",
     };
+  },
+
+  components: {
+    CharacterCardList,
+  },
+
+  computed: {
+    apiEndpoint() {
+      return api.baseUrl + `character`;
+    },
+  },
+
+  methods: {
+    fetchCharacters(endpoint = this.apiEndpoint) {
+      axios.get(endpoint).then((response) => {
+        console.log(response.data);
+        store.characters = response.data;
+      });
+    },
   },
 };
 </script>
@@ -16,7 +36,17 @@ export default {
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
+
+    <div class="row">
+      <input type="number" name="" v-model="n_character" class="form-control" />
+      <div class="btn btn-primary mt-2" @click="fetchCharacters()">
+        Genera personaggi
+      </div>
+      <CharacterCardList :characters="store.characters"></CharacterCardList>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+@use "/src/scss/general.scss";
+</style>
---
+++ src/components/CharacterCardList.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "GDR",
+      n_character: "",
+    };
+  },
+
+  props: {
+    characters: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="row row-cols-3 g-3">
+    <div class="col" v-for="character in characters">
+      <div class="card h-100">
+        <img :src="character.type.Image" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{ character.name }}</h5>
+          <p class="card-text">
+            {{ character.description }}
+          </p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <strong>Attacco: </strong>{{ character.attack }}
+          </li>
+          <li class="list-group-item">
+            <strong>Difesa: </strong>{{ character.defense }}
+          </li>
+          <li class="list-group-item">
+            <strong>Velocità: </strong>{{ character.speed }}
+          </li>
+          <li class="list-group-item">
+            <strong>Vita: </strong>{{ character.life }}
+          </li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
