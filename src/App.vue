9e5f76f97be509b8a936097f6baf33c251e01edb
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-04-17 15:49:47 +0200

    feat: base setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script></script>
+<script>
+import axios from "axios";
+import { api, store } from "./store";
+// import AppHeader from "./components/AppHeader.vue";
 
-<template></template>
+export default {
+  data() {
+    return {
+      store,
+      title: "GDR",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
 
 <style></style>
